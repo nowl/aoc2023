41ba7c7dc02f378ea5d49a6330b9d9b7076b4337
John Process <esologic@gmail.com>
2023-12-08 00:01:40 -0500

    day 7 part 2

---
+++ examples/day7p2.rs
@@ -0,0 +1,298 @@
+use std::{cmp::Ordering, fmt::Display, fs, path::Path};
+
+use anyhow::Error;
+use itertools::Itertools;
+use nom::IResult;
+
+#[derive(Debug, PartialEq, Hash, Eq)]
+enum Card {
+    Ace,
+    King,
+    Queen,
+    Joker,
+    Ten,
+    Nine,
+    Eight,
+    Seven,
+    Six,
+    Five,
+    Four,
+    Three,
+    Two,
+}
+
+impl Card {
+    fn value(&self) -> u8 {
+        use Card::*;
+        match self {
+            Ace => 12,
+            King => 11,
+            Queen => 10,
+            Ten => 9,
+            Nine => 8,
+            Eight => 7,
+            Seven => 6,
+            Six => 5,
+            Five => 4,
+            Four => 3,
+            Three => 2,
+            Two => 1,
+            Joker => 0,
+        }
+    }
+
+    fn first_char(&self) -> char {
+        use Card::*;
+        match self {
+            Ace => 'A',
+            King => 'K',
+            Queen => 'Q',
+            Ten => 'T',
+            Nine => '9',
+            Eight => '8',
+            Seven => '7',
+            Six => '6',
+            Five => '5',
+            Four => '4',
+            Three => '3',
+            Two => '2',
+            Joker => 'J',
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Hand {
+    cards: Vec<Card>,
+    points: i32,
+}
+
+impl Display for Hand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c0 = self.cards[0].first_char();
+        let c1 = self.cards[1].first_char();
+        let c2 = self.cards[2].first_char();
+        let c3 = self.cards[3].first_char();
+        let c4 = self.cards[4].first_char();
+        f.write_fmt(format_args!("{}{}{}{}{}", c0, c1, c2, c3, c4))
+    }
+}
+
+impl Hand {
+    fn is_five(&self) -> bool {
+        let counts = self.cards.iter().counts();
+        if let Some(n) = counts.get(&Card::Joker) {
+            counts.iter().any(|(_, &num)| num + n == 5) || *n == 5
+        } else {
+            counts.iter().any(|(_, &num)| num == 5)
+        }
+    }
+
+    fn is_four(&self) -> bool {
+        let counts = self.cards.iter().counts();
+        if let Some(n) = counts.get(&Card::Joker) {
+            counts
+                .iter()
+                .filter(|&(&c, _)| *c != Card::Joker)
+                .any(|(_, &num)| num + n == 4)
+        } else {
+            counts.iter().any(|(_, &num)| num == 4)
+        }
+    }
+
+    fn is_full_house(&self) -> bool {
+        let counts = self.cards.iter().counts();
+        if let Some(n) = counts.get(&Card::Joker) {
+            //AABBJ
+            *n == 1
+                && counts
+                    .iter()
+                    .filter(|&(&c, _)| *c != Card::Joker)
+                    .any(|(_, &num)| num + n == 3)
+                && counts.len() == 3
+        } else {
+            counts.iter().any(|(_, &num)| num == 3) && counts.len() == 2
+        }
+    }
+
+    fn is_three(&self) -> bool {
+        let counts = self.cards.iter().counts();
+        if let Some(n) = counts.get(&Card::Joker) {
+            //ABCJJ or ABCCJ
+            (*n == 1 || *n == 2)
+                && counts
+                    .iter()
+                    .filter(|&(&c, _)| *c != Card::Joker)
+                    .any(|(_, &num)| num + n == 3)
+                && counts.len() == 4
+        } else {
+            counts.iter().any(|(_, &num)| num == 3) && counts.len() == 3
+        }
+    }
+
+    fn is_two_pair(&self) -> bool {
+        let counts = self.cards.iter().counts();
+        let nums = counts.iter().map(|(_, &num)| num).sorted().collect_vec();
+        nums == [1, 2, 2]
+    }
+
+    fn is_one_pair(&self) -> bool {
+        let counts = self.cards.iter().counts();
+        if let Some(n) = counts.get(&Card::Joker) {
+            // ABCDJ
+            *n == 1
+                && counts
+                    .iter()
+                    .filter(|&(&c, _)| *c != Card::Joker)
+                    .any(|(_, &num)| num + n == 2)
+                && counts.len() == 5
+        } else {
+            let nums = counts.iter().map(|(_, &num)| num).sorted().collect_vec();
+            nums == [1, 1, 1, 2]
+        }
+    }
+
+    fn is_high_card(&self) -> bool {
+        let counts = self.cards.iter().counts();
+        counts.len() == 5
+    }
+
+    fn hand_value(&self) -> u8 {
+        if self.is_five() {
+            7
+        } else if self.is_four() {
+            6
+        } else if self.is_full_house() {
+            5
+        } else if self.is_three() {
+            4
+        } else if self.is_two_pair() {
+            3
+        } else if self.is_one_pair() {
+            2
+        } else if self.is_high_card() {
+            1
+        } else {
+            println!("{self:?}");
+            unreachable!()
+        }
+    }
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.value().cmp(&other.value())
+    }
+}
+
+impl PartialEq for Hand {
+    fn eq(&self, other: &Self) -> bool {
+        self.cards.iter().sorted().collect_vec() == other.cards.iter().sorted().collect_vec()
+    }
+}
+
+impl Eq for Hand {}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let val1 = self.hand_value();
+        let val2 = other.hand_value();
+        if val1 == val2 {
+            // go through each card
+            self.cards
+                .iter()
+                .zip(other.cards.iter())
+                .find_map(|(c1, c2)| {
+                    let comparison = c1.cmp(c2);
+                    if comparison == Ordering::Equal {
+                        None
+                    } else {
+                        Some(comparison)
+                    }
+                })
+                .unwrap_or(Ordering::Equal)
+        } else {
+            val1.cmp(&val2)
+        }
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn parse_card(i: &str) -> IResult<&str, Card> {
+    use Card::*;
+
+    let (i, c) = nom::character::complete::one_of("AKQJT98765432")(i)?;
+    let card = match c {
+        'A' => Ace,
+        'K' => King,
+        'Q' => Queen,
+        'J' => Joker,
+        'T' => Ten,
+        '9' => Nine,
+        '8' => Eight,
+        '7' => Seven,
+        '6' => Six,
+        '5' => Five,
+        '4' => Four,
+        '3' => Three,
+        '2' => Two,
+        _ => unreachable!(),
+    };
+    Ok((i, card))
+}
+
+fn parse_hand(i: &str) -> IResult<&str, Hand> {
+    use nom::character::complete::*;
+    use nom::multi::*;
+
+    let (i, cards) = count(parse_card, 5)(i)?;
+    let (i, _) = space1(i)?;
+    let (i, points) = i32(i)?;
+    let (i, _) = multispace1(i)?;
+
+    let hand = Hand { cards, points };
+
+    Ok((i, hand))
+}
+
+fn parse_input(i: &str) -> IResult<&str, Vec<Hand>> {
+    nom::multi::many1(parse_hand)(i)
+}
+
+fn main() -> Result<(), Error> {
+    let file = Path::new("data/d7p1.txt");
+    let contents = fs::read_to_string(file)?;
+    let data = parse_input(&contents);
+    let data = data.map_err(|err| err.map_input(|s| s.to_string()))?;
+    assert!(data.0 == "");
+    let mut hands = data.1;
+
+    hands.sort();
+
+    let score = hands
+        .iter()
+        .enumerate()
+        .map(|(i, h)| ((i as i32 + 1) * h.points) as u64)
+        .sum::<u64>();
+
+    /*
+    for hand in hands.into_iter().rev() {
+        println!("{hand}");
+    }
+     */
+
+    println!("{score}");
+
+    Ok(())
+}
